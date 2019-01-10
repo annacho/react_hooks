30840a2c9c977c1d0ec722a4f92aee19763d0a2a
annacho <annacho13@gmail.com>
2019-01-10 01:08:05 -0500

    Add useReducer hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
   const [submittedTodo, setSubmittedTodo] = useState(null);
-  const [todoList, setTodoList] = useState([]);
+  // const [todoList, setTodoList] = useState([]);
   // const [todoState, setTodoState] = useState({userInput: '', todoList: [] })
+
+  const todoListReducer = (state, action) => {
+    switch(action.type) {
+      case 'ADD':
+        return state.concat(action.payload);
+      case 'SET':
+        return action.payload;
+      case 'REMOVE':
+        return state.filter((todo) => todo.id !== action.payload);
+      default:
+        return state;
+    }
+  }
+
+  const [todoList, dispatch] = useReducer(todoListReducer, []);
 
   useEffect(() => {
     axios.get('https://react-hooks-319f3.firebaseio.com/todos.json').then(result => {
@@ -15,7 +30,7 @@ const todo = props => {
       for (const key in todoData) {
         todos.push({id: key, name: todoData[key].name})
       }
-      setTodoList(todos);
+      dispatch({type: 'SET', payload: todos});
     });
     return () => {
       console.log('Cleanup');
@@ -35,7 +50,7 @@ const todo = props => {
 
   useEffect(() => {
     if (submittedTodo) {
-    setTodoList(todoList.concat(submittedTodo));
+    dispatch({ type: 'ADD', payload: submittedTodo });
     }
   },
     [submittedTodo]
